a9e87d2220010d96d720624d13ef3b70e3834ae2
hicham ahendouz <ahendouz.dev@gmail.com>
2019-01-03 17:48:24 +0100

    implemet react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require("cors");
 require("dotenv").config({ path: ".env" });
-
 
 // the routes
 const users = require("../routes/api/users");
 const profile = require("../routes/api/profile");
 const post = require("../routes/api/post");
 
-
 const app = express();
-
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // Connecting to our database
 const db = require("./db");
-
 
 // passport middleware
 app.use(passport.initialize());
 require("./../utils")(passport);
 
-
 // welcome
 app.get("/", (req, res) => res.send("Welcome"));
-
 
 // User routers
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/post", post);
 
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true
+// };
+// app.use(cors());
+app.all("/", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 // listen to a specific port on our machine
 const port = process.env.PORT || 4000;
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "../components/Layout/Navbar/Navbar";
+import Home from "../components/Home";
+// import Signin from "../components/Auth/Signin";
+// import Signup from "../components/Auth/Signup";
+import Footer from "../components/Layout/Footer/Footer";
+
+export const Root = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        {/* <Route path="/signin" component={Signin} /> */}
+        {/* <Route path="/signup" component={Signup} /> */}
+      </Switch>
+      <Footer />
+    </Fragment>
+  </Router>
+);
